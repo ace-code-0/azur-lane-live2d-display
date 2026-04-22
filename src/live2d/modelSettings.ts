b0df144619ad429725bc65d22404ff13e2ff3985
ACEACE-code <2323180304@qq.com>
2026-04-22 18:50:49 +0800

    feat: 补充模型配置类型定义

--- src/live2d/modelSettings.ts
+++ src/live2d/modelSettings.ts
@@ -1,32 +1,148 @@
+export type ModelVariableConditionType = 1;
+export type ModelVariableAssignmentType = 2;
+export type ModelVariableType =
+  | ModelVariableConditionType
+  | ModelVariableAssignmentType;
+
+export type ModelVariable = {
+  Name: string;
+  Type: ModelVariableType;
+  Code: string;
+};
+
+export type ModelChoice = {
+  Text: string;
+  NextMtn: string;
+};
+
 export type ModelMotion = {
   File?: string;
   Name?: string;
-  VarFloats?: {
-    Name: string;
-    Type: number;
-    Code: string;
-  }[];
+  Sound?: string;
+  Text?: string;
+  Command?: string;
+  PostCommand?: string;
+  FileLoop?: boolean;
+  VarFloats?: ModelVariable[];
   Weight?: number;
+  Speed?: number;
+  MotionDuration?: number;
+  TextDuration?: number;
+  Choices?: ModelChoice[];
 };
 
 export type ModelHitArea = {
   Name: string;
+  Id?: string;
   Motion?: string;
 };
 
+export type ModelPhysicsV2 = {
+  File: string;
+  MaxWeight?: number;
+};
+
+export type ModelFileReferences = {
+  Moc: string;
+  Textures: string[];
+  Physics?: string;
+  PhysicsV2?: ModelPhysicsV2;
+  Motions?: Record<string, ModelMotion[]>;
+};
+
+export type ModelControllerItem = {
+  Name?: string;
+  Id: string;
+  Min?: number;
+  Max?: number;
+  Value?: number;
+  DefaultValue?: number;
+  BlendMode?: number;
+  Input?: number;
+  Axis?: number;
+  Factor?: number;
+  HitArea?: string;
+  ReleaseType?: number;
+  MaxMtn?: string;
+  Ids?: string[];
+};
+
+export type ModelKeyTriggerItem = {
+  Input: number;
+  DownMtn?: string;
+  UpMtn?: string;
+};
+
+export type ModelController = {
+  Enabled?: boolean;
+  Items?: ModelControllerItem[];
+};
+
+export type ModelIntervalController = ModelController & {
+  MinInterval?: number;
+  MaxInterval?: number;
+};
+
+export type ModelLipSyncController = ModelController & {
+  Gain?: number;
+};
+
+export type ModelKeyTriggerController = {
+  Enabled?: boolean;
+  Items?: ModelKeyTriggerItem[];
+};
+
+export type ModelArtmeshCullingController = {
+  DefaultMode?: number;
+};
+
+export type ModelIntimacySystemController = {
+  MaxValue?: number;
+  BonusActive?: number;
+};
+
+export type ModelControllers = {
+  ParamHit?: ModelController;
+  ParamLoop?: ModelController;
+  KeyTrigger?: ModelKeyTriggerController;
+  ParamTrigger?: ModelController;
+  AreaTrigger?: ModelController;
+  HandTrigger?: ModelController;
+  EyeBlink?: ModelIntervalController;
+  LipSync?: ModelLipSyncController;
+  MouseTracking?: ModelController & {
+    SmoothTime?: number;
+  };
+  AutoBreath?: ModelController;
+  ExtraMotion?: ModelController;
+  Accelerometer?: ModelController;
+  Microphone?: ModelController;
+  Transform?: ModelController;
+  FaceTracking?: ModelController;
+  HandTracking?: ModelController;
+  ParamValue?: ModelController;
+  PartOpacity?: ModelController;
+  ArtmeshOpacity?: ModelController;
+  ArtmeshColor?: ModelController;
+  ArtmeshCulling?: ModelArtmeshCullingController;
+  IntimacySystem?: ModelIntimacySystemController;
+};
+
+export type ModelOptions = {
+  ScaleFactor?: number;
+  AnisoLevel?: number;
+  PositionY?: number;
+  TexFixed?: boolean;
+  TexType?: number;
+};
+
 export type ModelSettings = {
-  FileReferences: {
-    Motions?: Record<string, ModelMotion[]>;
-  };
+  Version: number;
+  Type?: number;
+  FileReferences: ModelFileReferences;
   HitAreas?: ModelHitArea[];
-  Controllers?: {
-    ParamHit?: {
-      Items?: {
-        HitArea?: string;
-        MaxMtn?: string;
-      }[];
-    };
-  };
+  Controllers?: ModelControllers;
+  Options?: ModelOptions;
 };
 
 export async function loadModelSettings(
